39cb72ffe23d04a9a5f03e701939073e42b0ee96
Block Coin Digital <alejandrov@blockcoindigital.com>
2026-08-10 23:55:40 +0000

    test: verify Andaluzcoin encrypted wallet send identity

--- test/functional/test_runner.py
+++ test/functional/test_runner.py
@@ -136,6 +136,7 @@ BASE_SCRIPTS = [
     'wallet_andaluz_encryption_identity.py',
     'wallet_andaluz_passphrase_change_identity.py',
     'wallet_andaluz_message_signing_identity.py',
+    'wallet_andaluz_encrypted_send_identity.py',
     'p2p_orphan_handling.py',
     'wallet_basic.py',
     'feature_maxtipage.py',
