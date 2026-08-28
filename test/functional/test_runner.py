344f11f28847231b7eaf5dd99833fcd9e9af5835
Block Coin Digital <alejandrov@blockcoindigital.com>
2026-08-28 11:06:04 +0000

    test: verify Andaluzcoin wallet multisig rescan spend identity

--- test/functional/test_runner.py
+++ test/functional/test_runner.py
@@ -149,6 +149,7 @@ BASE_SCRIPTS = [
     'wallet_andaluz_watchonly_spend_restriction_identity.py',
     'wallet_andaluz_import_private_key_spend_identity.py',
     'wallet_andaluz_multisig_spend_identity.py',
+    'wallet_andaluz_multisig_rescan_spend_identity.py',
     'p2p_orphan_handling.py',
     'wallet_basic.py',
     'feature_maxtipage.py',
