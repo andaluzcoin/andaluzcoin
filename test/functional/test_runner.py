8373695df835a75555b78250d45f78696bf46106
Block Coin Digital <alejandrov@blockcoindigital.com>
2026-09-07 17:04:49 +0000

    test: verify Andaluzcoin wallet send RPC identity

--- test/functional/test_runner.py
+++ test/functional/test_runner.py
@@ -157,6 +157,7 @@ BASE_SCRIPTS = [
     'wallet_andaluz_fee_rate_identity.py',
     'wallet_andaluz_fee_estimation_identity.py',
     'wallet_andaluz_dust_change_identity.py',
+    'wallet_andaluz_send_rpc_identity.py',
     'p2p_orphan_handling.py',
     'wallet_basic.py',
     'feature_maxtipage.py',
