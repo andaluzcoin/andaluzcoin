500f47adf61a01bd06c1f2a880844ac03bffe630
Block Coin Digital <alejandrov@blockcoindigital.com>
2026-09-12 14:37:27 +0000

    test: verify Andaluzcoin wallet send PSBT identity

--- test/functional/test_runner.py
+++ test/functional/test_runner.py
@@ -159,6 +159,7 @@ BASE_SCRIPTS = [
     'wallet_andaluz_dust_change_identity.py',
     'wallet_andaluz_send_rpc_identity.py',
     'wallet_andaluz_sendall_identity.py',
+    'wallet_andaluz_send_psbt_identity.py',
     'p2p_orphan_handling.py',
     'wallet_basic.py',
     'feature_maxtipage.py',
